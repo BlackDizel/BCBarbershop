ea2e422fd02864ed8be3e636cbc7208a967c5cfa
BlackDizel <blackdizel@gmail.com>
2016-01-04 00:44:26 +0300

    add activity news details stub

---
+++ application/src/main/java/ru/byters/bcbarbershop/ui/activities/ActivityNewsDetails.java
@@ -0,0 +1,14 @@
+package ru.byters.bcbarbershop.ui.activities;
+
+import android.os.Bundle;
+
+import ru.byters.bcbarbershop.R;
+
+public class ActivityNewsDetails extends ActivityBase {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_details);
+    }
+}
