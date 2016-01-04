32ee4c249e52ffa15d76e5c1b4300f5464a339e7
BlackDizel <blackdizel@gmail.com>
2016-01-04 16:47:46 +0300

    add actionbar home onclick listener

--- application/src/main/java/ru/byters/bcbarbershop/ui/activities/ActivityNewsDetails.java
+++ application/src/main/java/ru/byters/bcbarbershop/ui/activities/ActivityNewsDetails.java
@@ -1,6 +1,9 @@
 package ru.byters.bcbarbershop.ui.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.ViewGroup;
 
 import ru.byters.bcbarbershop.R;
 import ru.byters.bcbarbershop.controllers.Controller;
@@ -9,17 +12,29 @@ import ru.byters.bcbarbershop.controllers.ControllerNewsActivity;
 public class ActivityNewsDetails extends ActivityBase {
 
     public static final String INTENT_EXTRA_KEY = "id";
-    //todo add share action
-    //todo add like action (если новость - фотография другого пользователя, дать возможность лайкнуть)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         new ControllerNewsActivity().setUI(
                 (Controller) getApplicationContext()
-                , findViewById(R.id.flRoot)
+                , (ViewGroup) findViewById(R.id.flRoot)
                 , getIntent().getIntExtra(INTENT_EXTRA_KEY, 0));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
