0ae908423654432311cb0ac891bae9b3757627f6
BlackDizel <blackdizel@gmail.com>
2016-01-04 17:05:52 +0300

    implement news details layouts

--- application/src/main/java/ru/byters/bcbarbershop/controllers/ControllerNewsActivity.java
+++ application/src/main/java/ru/byters/bcbarbershop/controllers/ControllerNewsActivity.java
@@ -1,21 +1,35 @@
 package ru.byters.bcbarbershop.controllers;
 
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.text.SimpleDateFormat;
+
+import ru.byters.bcbarbershop.R;
 import ru.byters.bcbarbershop.dataclasses.News;
 
 public class ControllerNewsActivity {
 
-    public void setUI(Controller controller, View view, int id) {
+    public void setUI(Controller controller, ViewGroup view, int id) {
         News news = controller.controllerNews.getNews().getItemById(id);
         if (news == null) return;
 
+        View v;
         if (!TextUtils.isEmpty(news.PhotoURI)) {
-            //todo set photo
+            v = LayoutInflater.from(controller).inflate(R.layout.view_news_details_photo, view);
+            ImageLoader.getInstance().displayImage(news.PhotoURI, (ImageView) v.findViewById(R.id.ivPhoto));
         } else {
-            //todo set text
+            v = LayoutInflater.from(controller).inflate(R.layout.view_news_details_text, view);
+            ((TextView) v.findViewById(R.id.tvDescription)).setText(news.Description);
         }
 
+        ((TextView) v.findViewById(R.id.tvTitle)).setText(news.Title);
+        ((TextView) v.findViewById(R.id.tvDate)).setText(new SimpleDateFormat("DD MMMM").format(news.NewsDate));
     }
 }
---
+++ application/src/main/java/ru/byters/bcbarbershop/ui/views/ImageViewSquare.java
@@ -0,0 +1,25 @@
+package ru.byters.bcbarbershop.ui.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class ImageViewSquare extends ImageView {
+    public ImageViewSquare(Context context) {
+        super(context);
+    }
+
+    public ImageViewSquare(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageViewSquare(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //noinspection SuspiciousNameCombination
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
