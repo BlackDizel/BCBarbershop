28c94236820fc77e64b579c2ae2e6cedfda15e5f
BlackDizel <blackdizel@gmail.com>
2016-01-05 17:25:48 +0300

    change linearlayoutmanager with gridlayoutmanager. add itemdecoration

--- application/src/main/java/ru/byters/bcbarbershop/ui/fragments/FragmentMenu.java
+++ application/src/main/java/ru/byters/bcbarbershop/ui/fragments/FragmentMenu.java
@@ -2,7 +2,7 @@ package ru.byters.bcbarbershop.ui.fragments;
 
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,15 +24,42 @@ public class FragmentMenu extends FragmentBase {
 
         rvMenu = (RecyclerView) rootView.findViewById(R.id.rvMainMenu);
         rvMenu.setHasFixedSize(true);
-        rvMenu.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
+        rvMenu.setLayoutManager(new GridLayoutManager(
+                rootView.getContext()
+                , rootView.getContext().getResources().getInteger(R.integer.menu_columns_num)));
         rvMenu.setAdapter(new AdapterMainMenu(rootView.getContext()));
-        rvMenu.addItemDecoration(new RecyclerView.ItemDecoration() {
-            @Override
-            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-                int margin = (int) getResources().getDimension(R.dimen.item_menu_margin);
-                outRect.set(margin, margin, margin, 0);
-            }
-        });
+        rvMenu.addItemDecoration(new ItemsDecorator(rootView.getContext().getResources().getInteger(R.integer.menu_columns_num)));
         return rootView;
     }
+
+    private class ItemsDecorator extends RecyclerView.ItemDecoration {
+        private final int columns;
+
+        public ItemsDecorator(int columns) {
+            this.columns = columns;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int space = (int) getResources().getDimension(R.dimen.item_menu_margin);
+            int pos = parent.getChildAdapterPosition(view);
+
+            if (columns == 2) {
+                //[4]x[2][2]x[4]
+                if (pos % 2 == 0)
+                    outRect.set(4 * space, 4 * space, 2 * space, 0);
+                else
+                    outRect.set(2 * space, 4 * space, 4 * space, 0);
+            } else if (columns == 3) {
+                //[6]x[0][3]x[3][0]x[6]
+                if (pos % 3 == 0)
+                    outRect.set(6 * space, 4 * space, 0, 0);
+                else if (pos % 3 == 1)
+                    outRect.set(3 * space, 4 * space, 3 * space, 0);
+                else if (pos % 3 == 2)
+                    outRect.set(0, 4 * space, 6 * space, 0);
+            }
+        }
+    }
+
 }
