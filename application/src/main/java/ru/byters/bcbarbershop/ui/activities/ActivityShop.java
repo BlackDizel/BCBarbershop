bd57b08359563925165e4ee1070d3695d1b1323c
BlackDizel <blackdizel@gmail.com>
2016-01-08 20:49:08 +0300

    use adapter

--- application/src/main/java/ru/byters/bcbarbershop/ui/activities/ActivityShop.java
+++ application/src/main/java/ru/byters/bcbarbershop/ui/activities/ActivityShop.java
@@ -1,11 +1,15 @@
 package ru.byters.bcbarbershop.ui.activities;
 
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import ru.byters.bcbarbershop.R;
+import ru.byters.bcbarbershop.controllers.Controller;
+import ru.byters.bcbarbershop.controllers.adapters.AdapterProducts;
 
 public class ActivityShop extends ActivityBase {
 
@@ -17,15 +21,20 @@ public class ActivityShop extends ActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        AdapterProducts adapter = ((Controller) getApplicationContext()).adapterProducts;
+        adapter.setCategoryID(getIntent().getIntExtra(INTENT_EXTRA_ID, -1));
+        adapter.setViews((SwipeRefreshLayout) findViewById(R.id.srlProducts));
+
         rvMenu = (RecyclerView) findViewById(R.id.rvShop);
         rvMenu.setHasFixedSize(true);
-
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         rvMenu.setLayoutManager(mLayoutManager);
-
-        //  rvMenu.setAdapter(((Controller) getApplicationContext()).adapterProducts);
+        rvMenu.setAdapter(adapter);
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
