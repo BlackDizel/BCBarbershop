d169522ff6ac67b6cc817fa461196ff8ce6438ba
BlackDizel <blackdizel@gmail.com>
2016-01-13 23:55:44 +0300

    add method to get categories without parent

--- application/src/main/java/ru/byters/bcbarbershop/models/ModelCategories.java
+++ application/src/main/java/ru/byters/bcbarbershop/models/ModelCategories.java
@@ -2,6 +2,7 @@ package ru.byters.bcbarbershop.models;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
 
@@ -11,6 +12,7 @@ import ru.byters.bcbarbershop.dataclasses.Category;
 public class ModelCategories {
     public static String tablename;
     private ArrayList<Category> data;
+    private ArrayList<Category> dataTopLevel;
 
     public ModelCategories(Context context, ArrayList<Category> l) {
         data = l;
@@ -28,14 +30,35 @@ public class ModelCategories {
         return 0;
     }
 
+    public int getSizeTopLevel() {
+        if (data != null) {
+            if (dataTopLevel == null || dataTopLevel.size() == 0) {
+                dataTopLevel = new ArrayList<>();
+                for (Category category : data)
+                    if (category.getParentCategory() == 0)
+                        dataTopLevel.add(category);
+            }
+            return dataTopLevel.size();
+        }
+        return 0;
+    }
+
     public Category getItem(int pos) {
         if ((pos < data.size()) && (pos >= 0))
             return data.get(pos);
         return null;
     }
 
+    @Nullable
+    public Category getItemTopLevel(int pos) {
+        if (dataTopLevel != null && (pos < dataTopLevel.size()) && (pos >= 0))
+            return dataTopLevel.get(pos);
+        return null;
+    }
+
     public void setData(@NonNull Context context, @NonNull ArrayList<Category> result) {
         data = result;
+        dataTopLevel = null;
         ControllerStorage.writeObjectToFile(context, data, ControllerStorage.CATEGORIES);
     }
 
