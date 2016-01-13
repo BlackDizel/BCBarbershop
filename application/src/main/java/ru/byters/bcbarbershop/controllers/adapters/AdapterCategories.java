63a648ffa7122bcb9ebf6ee6b04d07a7d8955df8
BlackDizel <blackdizel@gmail.com>
2016-01-13 23:07:25 +0300

    add phone item

--- application/src/main/java/ru/byters/bcbarbershop/controllers/adapters/AdapterCategories.java
+++ application/src/main/java/ru/byters/bcbarbershop/controllers/adapters/AdapterCategories.java
@@ -1,7 +1,10 @@
 package ru.byters.bcbarbershop.controllers.adapters;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,15 +20,18 @@ import ru.byters.bcbarbershop.ui.activities.ActivityShop;
 public class AdapterCategories extends RecyclerView.Adapter<AdapterCategories.ViewHolder> {
     int[] colors;
     private Controller controller;
+    private String phone;
 
     public AdapterCategories(Controller controller) {
         this.controller = controller;
+        phone = controller.controllerBarbershopInfo.model.getData() != null ?
+                controller.controllerBarbershopInfo.model.getData().Phone : null;
         colors = controller.getResources().getIntArray(R.array.main_menu_colors);
     }
 
     @Override
     public int getItemCount() {
-        return controller.categories.getSize();
+        return controller.categories.getSize() + (TextUtils.isEmpty(phone) ? 0 : 1);
     }
 
     @Override
@@ -39,28 +45,58 @@ public class AdapterCategories extends RecyclerView.Adapter<AdapterCategories.Vi
                 LayoutInflater.from(parent.getContext()).inflate(R.layout.view_menu_main_item, parent, false));
     }
 
+    public void updateFooter(@Nullable String phone) {
+        boolean isPhoneExist = false;
+        if (!TextUtils.isEmpty(this.phone)) isPhoneExist = true;
+        if (!TextUtils.isEmpty(phone)) {
+            this.phone = phone;
+            if (isPhoneExist) notifyItemChanged(getItemCount() - 1);
+            else notifyItemInserted(getItemCount() - 1);
+        }
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder implements OnClickListener {
         private TextView tvItem;
+        private TextView tvDesc;
+        private View view;
         private int categoryID;
 
         public ViewHolder(View v) {
             super(v);
             tvItem = (TextView) v.findViewById(R.id.tvData);
+            tvDesc = (TextView) v.findViewById(R.id.tvDescription);
+            this.view = v;
             v.setOnClickListener(this);
         }
 
         public void setData(int position) {
-            Category item = controller.categories.getItem(position);
-            tvItem.setText(item.getTitle().toUpperCase());
-            tvItem.setBackgroundColor(colors[position % colors.length]);
-            categoryID = item.getCategoryID();
+            if (!TextUtils.isEmpty(phone) && position == getItemCount() - 1) {
+                tvItem.setText(controller.getResources().getString(R.string.call_to_barbershop));
+                tvDesc.setText("");
+            } else {
+                Category item = controller.categories.getItem(position);
+                tvItem.setText(item.getTitle().toUpperCase());
+                tvDesc.setText(item.getDescription());
+                categoryID = item.getCategoryID();
+            }
+            view.setBackgroundColor(colors[position % colors.length]);
         }
 
         @Override
         public void onClick(View v) {
-            Intent i = new Intent(v.getContext(), ActivityShop.class);
-            i.putExtra(ActivityShop.INTENT_EXTRA_ID, categoryID);
-            v.getContext().startActivity(i);
+
+            int position = getAdapterPosition();
+            if (position == RecyclerView.NO_POSITION) return;
+
+            if (!TextUtils.isEmpty(phone) && position == getItemCount() - 1) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phone));
+                if (intent.resolveActivity(controller.getPackageManager()) != null)
+                    v.getContext().startActivity(intent);
+            } else {
+                Intent i = new Intent(v.getContext(), ActivityShop.class);
+                i.putExtra(ActivityShop.INTENT_EXTRA_ID, categoryID);
+                v.getContext().startActivity(i);
+            }
         }
     }
 }
