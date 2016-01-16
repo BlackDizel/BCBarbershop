14ac36dba9297a8dae0289fc90ab92c4e6128950
BlackDizel <blackdizel@gmail.com>
2016-01-16 21:08:38 +0300

    set product info image and text. add on back pressed listener

--- application/src/main/java/ru/byters/bcbarbershop/ui/activities/ActivityProductInfo.java
+++ application/src/main/java/ru/byters/bcbarbershop/ui/activities/ActivityProductInfo.java
@@ -2,8 +2,16 @@ package ru.byters.bcbarbershop.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import ru.byters.bcbarbershop.R;
+import ru.byters.bcbarbershop.controllers.Controller;
+import ru.byters.bcbarbershop.dataclasses.Product;
 
 public class ActivityProductInfo extends ActivityBase {
 
@@ -16,6 +24,24 @@ public class ActivityProductInfo extends ActivityBase {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Product product = ((Controller) getApplicationContext()).controllerProducts.getProducts().getProductWithId(getIntent().getIntExtra(INTENT_EXTRA_PRODUCT_ID, -1));
+
+        if (product != null) {
+            ((TextView) findViewById(R.id.tvTitle)).setText(product.getTitle());
+            ((TextView) findViewById(R.id.tvDescription)).setText(product.getDescription());
+
+            ImageView ivProduct = (ImageView) findViewById(R.id.ivProduct);
+            if (!TextUtils.isEmpty(product.getPhotoURI()))
+                ImageLoader.getInstance().displayImage(product.getPhotoURI(), ivProduct);
+        }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
 }
