39b9a78aa89487f12e07a5d070e5503112cf001f
BlackDizel <blackdizel@gmail.com>
2016-01-17 16:52:32 +0300

    add missing maestro class

---
+++ application/src/main/java/ru/byters/bcbarbershop/dataclasses/Maestro.java
@@ -0,0 +1,11 @@
+package ru.byters.bcbarbershop.dataclasses;
+
+import java.io.Serializable;
+
+public class Maestro implements Serializable {
+    public int MaestroID;
+    public String FIO;
+    public String Description;
+    public String PhotoURI;
+    private int id;
+}
